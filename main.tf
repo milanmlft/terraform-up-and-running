e0c8f6aa9d1da69ca0e268c5eddff5f43943fa82
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-28 16:52:30 +0100

    Add an ALB listener

--- main.tf
+++ main.tf
@@ -51,6 +51,23 @@ resource "aws_lb" "example" {
   subnets            = data.aws_subnets.default.ids
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
 data "aws_vpc" "default" {
   default = true
 }
