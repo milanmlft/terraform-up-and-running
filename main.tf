dff906c0423e126d2ebe45628a249b033dec4290
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-28 17:02:33 +0100

    Add target group for the ALB

--- main.tf
+++ main.tf
@@ -24,6 +24,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.id
   vpc_zone_identifier  = data.aws_subnets.default.ids
 
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 10
 
@@ -66,6 +69,23 @@ resource "aws_lb_listener" "http" {
       message_body = "404: page not found"
       status_code  = 404
     }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
