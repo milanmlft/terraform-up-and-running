4b3ef17a32784307627900dc4c174d3659673559
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-28 16:07:25 +0100

    Format main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-	region = "us-east-2"
-	profile = "milanmlft"
+  region  = "us-east-2"
+  profile = "milanmlft"
 }
 
 resource "aws_launch_configuration" "example" {
-	image_id = "ami-0fb653ca2d3203ac1"
-	instance_type = "t2.micro"
-	securitysecurity_groups = [aws_security_group.instance.id]
+  image_id                = "ami-0fb653ca2d3203ac1"
+  instance_type           = "t2.micro"
+  securitysecurity_groups = [aws_security_group.instance.id]
 
-	user_data = <<-EOF
+  user_data = <<-EOF
 				#!/bin/bash
 				echo "Hello, World" > index.html
 				nohup busybox httpd -f -p ${var.server_port} &
@@ -16,36 +16,36 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
-	launch_configuration = aws_launch_configuration.example.id
+  launch_configuration = aws_launch_configuration.example.id
 
-	min_size = 2
-	max_size = 10
+  min_size = 2
+  max_size = 10
 
-	tag {
-		key = "Name"
-		value = "terraform-example-asg"
-		propagate_at_launch = true
-	}
+  tag {
+    key                 = "Name"
+    value               = "terraform-example-asg"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_security_group" "instance" {
-	name = "terraform-example-instance"
+  name = "terraform-example-instance"
 
-	ingress {
-		from_port = var.server_port
-		to_port = var.server_port
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 variable "server_port" {
-	description = "The port the server will use for HTTP requests"
-	type = number
-	default = 8080
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
 
 output "public_ip" {
-	value = aws_instance.example.public_ip
-	description = "The public IP address of the web server"
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
