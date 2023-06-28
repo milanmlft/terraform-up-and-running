ea44aac6b1d5db5145752727661eff424b9b3c0f
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-28 16:48:51 +0100

    Add an Application Load Balancer (ALB)

--- main.tf
+++ main.tf
@@ -45,6 +45,12 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+}
+
 data "aws_vpc" "default" {
   default = true
 }
