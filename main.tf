a94a2e2543cd39c5afbbd0c26093bcf4094d00ff
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-29 11:18:37 +0100

    Move the S3 backend configuration to `remote/main.tf`
    Wasn't entirely sure where to put this.

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "2023-06-29-terraform-up-and-running-state"
-    key    = "remote/terraform.tfstate"
-    region = "us-east-2"
-
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region  = "us-east-2"
   profile = "milanmlft"
