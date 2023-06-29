b62b28e66d646a9968cd399ed7b95cc08a20e21c
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-29 11:14:27 +0100

    Add remote outputs

--- remote/main.tf
+++ remote/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region  = "us-east-2"
   profile = "milanmlft"
 }
 
@@ -49,4 +49,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
