f082e124d808f70bf6cb8959d77d07cd93e08b88
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-29 14:51:47 +0100

    Set up the database

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket = "2023-06-29-terraform-up-and-running-state"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "milanmlft"
+}
+
+resource "aws_db_instance" "example" {
+  identifier          = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10 # GB
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  # How should we set the username and password?
+  username = var.db_username
+  password = var.db_password
+}
