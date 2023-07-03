8de05785900963087cff54d63932a104d1ce5090
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-03 17:24:31 +0100

    Use separate ingress and egress resources

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -34,13 +34,16 @@ resource "aws_autoscaling_group" "example" {
 
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
+}
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_http_inbound-instance" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb" "example" {
@@ -102,22 +105,26 @@ resource "aws_lb_target_group" "asg" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  # Allow all outbound requests
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 data "aws_vpc" "default" {
