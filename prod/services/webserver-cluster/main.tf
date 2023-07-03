fcdc1892608d5eb5cb0ff03ff7ca16f01d541370
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-03 14:15:00 +0100

    Set variable values for the webserver environments

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -15,4 +15,8 @@ provider "aws" {
 
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
+
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "2023-06-29-terraform-up-and-running-state"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
 }
