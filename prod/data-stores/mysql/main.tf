68c0b7bbbaf5bcb5b2eb7b1e661384dee8330741
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-03 16:07:46 +0100

    Add production database

---
+++ prod/data-stores/mysql/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket = "2023-06-29-terraform-up-and-running-state"
+    key    = "prod/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "milanmlft"
+}
+
+resource "aws_db_instance" "example" {
+  identifier          = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10 # GB
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = var.db_name
+  username = var.db_username
+  password = var.db_password
+}
