18828f9fcf1582765cee5b013b137501a23d59e4
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-03 13:47:33 +0100

    Also move terraform setup to the root modules

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "2023-06-29-terraform-up-and-running-state"
-    key    = "stage/services/webserver-cluster/terraform.tfstate"
-    region = "us-east-2"
-
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
-}
-
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0fb653ca2d3203ac1"
   instance_type   = "t2.micro"
